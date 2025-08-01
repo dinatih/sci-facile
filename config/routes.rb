0380a7397abd18b1305d53fbf61ad95e7480c92f
dinatih <dinatih@gmail.com>
2025-08-01 05:21:35 +0000

    Add welcome controller, update root route, and create homepage view with navigation and sections

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "application#index"
+  root "welcome#index"
 
   devise_for :admin_users
   authenticate :admin_user do
--- spec/system/sci_facile_spec.rb
+++ spec/system/sci_facile_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "SCI Facile", type: :system do
   scenario "Public access to the homepage" do
     visit "/"
     sleep 20.seconds
-    expect(page).to have_content("Welcome to the Application!")
+    expect(page).to have_content("Gérez votre SCI en toute simplicité")
   end
 end
