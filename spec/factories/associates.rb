36223469bb353fa413c265794eb95297c3ce69b9
dinatih <dinatih@gmail.com>
2025-08-06 23:50:29 +0000

    Add factories for associates and companies to enhance test data generation

---
+++ spec/factories/associates.rb
@@ -0,0 +1,29 @@
+FactoryBot.define do
+  factory :associate do
+    company
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email(name: "#{first_name} #{last_name}") }
+    shares_count { rand(1..1000) }
+    initial_contribution { rand(1000..50000) }
+    current_account_balance { rand(-5000..10000) }
+    password { 'password' }
+    password_confirmation { 'password' }
+
+    trait :major_associate do
+      shares_count { rand(500..1000) }
+      initial_contribution { rand(25000..100000) }
+      current_account_balance { rand(0..25000) }
+    end
+
+    trait :minor_associate do
+      shares_count { rand(1..100) }
+      initial_contribution { rand(1000..10000) }
+      current_account_balance { rand(-1000..5000) }
+    end
+
+    trait :with_negative_balance do
+      current_account_balance { rand(-10000..-1000) }
+    end
+  end
+end
---
+++ spec/factories/companies.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+  factory :company do
+    name { "SCI #{Faker::Company.name}" }
+
+    trait :with_associates do
+      after(:create) do |company|
+        create_list(:associate, 2, company: company)
+      end
+    end
+
+    trait :with_properties do
+      after(:create) do |company|
+        create_list(:property, 1, company: company)
+      end
+    end
+
+    trait :complete do
+      after(:create) do |company|
+        create_list(:associate, 3, company: company)
+        # create_list(:property, 2, company: company)
+      end
+    end
+  end
+end
