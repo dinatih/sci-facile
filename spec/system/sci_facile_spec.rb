f63a5790ba827edf56179ae7ba46202a59fd4a24
dinatih <dinatih@gmail.com>
2025-08-14 08:02:26 +0000

    Refactor associate registration in system tests for clarity and consistency

--- spec/system/sci_facile_spec.rb
+++ spec/system/sci_facile_spec.rb
@@ -5,28 +5,20 @@ RSpec.describe "SCI Facile", type: :system do
     click_link "Connexion"
     click_link "S'inscrire"
 
-    expect(page).to have_current_path(new_associate_registration_path)
-    expect(page).to have_content("S'inscrire")
     fill_in "Nom de votre SCI", with: company_name
     fill_in "Prénom", with: first_name
     fill_in "Nom", with: last_name
     fill_in "Email", with: email
     fill_in "Mot de passe", with: password
     fill_in "Confirmation du Mot de passe", with: password
-    click_button "Créer ce/cette Associé(e)"
-    puts page.body if page.has_content?("error")
-    expect(page).to have_current_path(company_associates_path(Company.last))
-    expect(page).to have_content(first_name)
+    click_button "Enregistrer cet Associé(e)"
   end
 
   scenario "Guide touristique" do
     Rails.application.load_seed
     # Seed completed: 1 admin users,5 companies, 15 associates, 20 properties, 10 tenants,
     #                 60 financial operations, 10 general meetings created.
-
     visit "/"
-    expect(page).to have_content("Gérez votre SCI en toute simplicité")
-
     current_company = Company.last
     visit company_path(current_company)
     visit company_associates_path(current_company)
@@ -35,25 +27,13 @@ RSpec.describe "SCI Facile", type: :system do
     visit company_tenants_path(current_company)
     visit company_general_meetings_path(current_company)
 
-    # expect(page).to have_content("Gérez votre SCI en toute simplicité")
-    # sleep 5
-    # register_associate(
-    #   company_name: "Eloidine",
-    #   first_name: "David",
-    #   last_name: "Herelle",
-    #   email: "david.herelleh@eloidine.com",
-    #   password: "password"
-    # )
-
-
-
-
-
-    # # Add a property
-    # click_link "Ajouter un bien"
-    # fill_in "Nom du bien", with: "Maison de vacances"
-    # fill_in "Adresse", with: "123 Rue de la Paix, Paris"
-    # fill_in "Valeur du bien", with: "250000"
-    # click_button "Créer Bien"
+    visit "/"
+    register_associate(
+      company_name: "Eloidine",
+      first_name: "David",
+      last_name: "Herelle",
+      email: "david.herelleh@eloidine.com",
+      password: "password"
+    )
   end
 end
