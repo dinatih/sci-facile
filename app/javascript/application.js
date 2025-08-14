254ac5f41c49eaf1aa4e189060786ee22ff7a452
dinatih <dinatih@gmail.com>
2025-08-14 09:59:36 +0000

    Refactor FontAwesome icon imports for clarity and consolidate comments

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,25 +5,17 @@ import * as bootstrap from "bootstrap"
 
 import "./chart/finance_chart"
 
-// Import FontAwesome core
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-
-// Import all solid icons
 import * as solidIcons from '@fortawesome/free-solid-svg-icons'
-// Import all regular icons
 import * as regularIcons from '@fortawesome/free-regular-svg-icons'
 
-// Add all solid icons to the library
+// Add all icons to the library
 const solidIconList = Object.keys(solidIcons)
   .filter(key => key.startsWith('fa'))
   .map(key => solidIcons[key])
-
-// Add all regular icons to the library
 const regularIconList = Object.keys(regularIcons)
   .filter(key => key.startsWith('fa'))
   .map(key => regularIcons[key])
-
 library.add(...solidIconList, ...regularIconList)
-
 // Tell FontAwesome to watch the DOM and add the SVGs when it detects icon markup
 dom.watch()
