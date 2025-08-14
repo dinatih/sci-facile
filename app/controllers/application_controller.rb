fca10770989e1be52311bd97ee9b96f23c19282f
dinatih <dinatih@gmail.com>
2025-08-14 01:00:15 +0000

    Remove unused index action from ApplicationController for cleaner code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  def index
-    # This action can be used to render a default view or redirect to another action.
-    render html: "Welcome to the Application!", layout: "application"
-  end
 end
